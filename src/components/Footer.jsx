d62859895ca3899cf9c158b101c897c3ed1f818f
Akshansh <akshanshkul7830@gmail.com>
2026-09-22 00:07:57 +0530

    update

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -95,8 +95,8 @@ export default function Footer() {
             &copy; {new Date().getFullYear()} {site.name}. All rights reserved.
           </p>
           <p>
-            Site designed by <a href="https://www.gradox.in" target="_blank" rel="noreferrer" className="font-semibold text-gold">Gradox [School Management Software]</a>
-          </p>
+            Site designed by <a href="https://www.gradox.in" target="_blank" rel="noreferrer" className="font-semibold text-gold">Gradox</a> 
+            <span>[School Management Software]</span></p>
           <p>{site.affiliation}</p>
         </div>
       </div>
